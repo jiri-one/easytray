26091d21fda2bfd2315f3205b477ec9afb791d98
jiri-one <nemec@jiri.one>
2023-11-26 21:06:36 +0100

    improved tests and add descriptions for them

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,13 @@
 from PIL import Image
 from pathlib import Path
+from subprocess import run
 
 # pytest imports
 import pytest
 
 # internal imports
 from easytray.dbus_backends import utils
+
 install_icon_to_xdg_data_home = utils.install_icon_to_xdg_data_home
 
 
@@ -36,12 +38,15 @@ SVG_IMAGE = """
 </svg>
 """
 
+
 @pytest.fixture
 def fake_xdg_data_home(tmp_path):
     def internal_fake_function():
         """Simple function to return fake XDG_DATA_HOME directory"""
         return tmp_path
+
     return internal_fake_function
+
 
 @pytest.fixture
 def image_svg(tmp_path):
@@ -54,23 +59,41 @@ def image_svg(tmp_path):
 def image_png(tmp_path):
     width = 100
     height = 100
-    img_path = tmp_path / "image.png"
+    img_path = tmp_path / "test_image.png"
     img = Image.new(mode="RGB", size=(width, height), color="red")
     img.save(img_path, bitmap_format="png")
     return img_path
 
 
 def test_install_icon_to_xdg_data_home_png(image_png):
+    """
+    In this test, we are testing the installation of an icon in PNG format. The system tool, or rather the command, xdg-icon-resource is used directly for the installation and therefore it is necessary to uninstall the icon again after successful installation. This makes the test a bit more complicated.
+    """
     new_icon_path_str = install_icon_to_xdg_data_home(
         icon_path=image_png, icon_size=100
     )
-    new_icon_path = Path(new_icon_path_str)
-    assert new_icon_path.is_dir()
-    assert (new_icon_path / "image.png").exists()
+    new_icon_path = Path(new_icon_path_str) / "test_image.png"
+    assert new_icon_path.exists()
+    # because this test operates with real linux commands, it is hard to test it without changing real folders
+    # so we need to clean the mess ...
+    process = run(
+        [
+            "xdg-icon-resource",
+            "uninstall",
+            "--novendor",
+            "--size",
+            "100",
+            "test_image",
+        ]
+    )
+    assert not new_icon_path.exists()
 
 
 def test_install_icon_to_xdg_data_home_svg(image_svg, fake_xdg_data_home, monkeypatch):
-    monkeypatch.setattr(utils, 'get_xdg_data_home', fake_xdg_data_home)
+    """
+    In this test, we are testing the installation of an icon in SVG format. Because the standard Linux tool for installing user icons, xdg-icon-resource, still does not support the SVG format, these icons have to be installed manually -> copied to the correct location and then restored to the cache. So during the test we can mock the directory where it is copied to, this makes the test easier.
+    """
+    monkeypatch.setattr(utils, "get_xdg_data_home", fake_xdg_data_home)
     new_icon_path_str = install_icon_to_xdg_data_home(icon_path=image_svg)
     new_icon_path = Path(new_icon_path_str)
     assert new_icon_path.is_dir()
