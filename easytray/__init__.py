5c801d360f9d10bfd03db8e959c8421160f9fdfa
jiri-one <nemec@jiri.one>
2023-11-27 10:03:11 +0100

    added install_icon_to_xdg_data_home to import

--- easytray/__init__.py
+++ easytray/__init__.py
@@ -1,4 +1,4 @@
-from .dbus_backends import get_dbus_backend
+from .dbus_backends import get_dbus_backend, install_icon_to_xdg_data_home
 from .menu import EasyTrayMenu
 
-__all__ = ["get_dbus_backend", "EasyTrayMenu"]
+__all__ = ["get_dbus_backend", "EasyTrayMenu", "install_icon_to_xdg_data_home"]
